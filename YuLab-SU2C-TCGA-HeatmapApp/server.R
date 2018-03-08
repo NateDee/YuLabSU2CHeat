241688a79ac2b5d763405320ceda689d4f60d41d
NateDee <35738092+NateDee@users.noreply.github.com>
2018-03-08 19:35:17 +0000

    Lots of updates
    User heatmap generation function, datasets summary tab, instructions tab etc

--- YuLab-SU2C-TCGA-HeatmapApp/server.R
+++ YuLab-SU2C-TCGA-HeatmapApp/server.R
@@ -4,6 +4,89 @@
 
 server <- function(input,output) {
 	
+	#Create "Instructions" panel:
+	output$text1 <- renderText(
+		{ paste( 
+		'<b>',
+			'<ol>',
+				'<li style="margin:10px 0";>To visualize how your gene set is regulated in prostate disease progression, start by selecting a database to query.</li>',
+				'<li style="margin:10px 0";>To see what each database contains, navigate to the "Datasets Summaries." tab</li>',
+				'<li style="margin:10px 0";>Upload your gene list or choose a list from the "test_gene_sets" folder. Your uploaded gene list must be in the following format:</li>',
+			'</ol>',	
+		'</b>'
+		)}
+	)
+	
+	output$text2 <- renderText(
+		{ paste( 
+		'<p>',
+			'<ul>',
+				'<li style="margin:10px 0";>The first row must contain headers (See Row 1 to the left).</li>',
+				'<li style="margin:10px 0";>The first column (Column A) must contain gene names (gene symbols).</li>',
+				'<li style="margin:10px 0";>If you only have a list of genes, duplicate row one (as shown in Column B).</li>',
+				'<li style="margin:10px 0";>If you have other data like gene expression measurements, these can be left as they are and you don\'t need to duplicate column 1.</li>',
+				'<li style="margin:10px 0";>To make a heatmap of your own data, see the instructions below under "Create a heatmap of your data!"</li>',
+				'<b><li style="margin:10px 0";>Save your data in the "Text (Tab delimited) (*.txt)" format.</li></b>',
+			'</ul>',	
+		'</p>'
+		)}
+	)
+	output$text3 <- renderText(
+		{ paste( 
+		'<b>',
+			'<ol start="4">',
+				'<li style="margin:10px 0";>The "Heatmap" tab will now display the expression of your genes in the tissues of the selected database.</li>',
+				'<li style="margin:10px 0";>By default, these genes will be in the same order as your input list.</li>',
+				'<li style="margin:10px 0";>If you would like to see how these genes correlate with a gene of interest, type in your gene (or genes) in the side bar.</li>',
+				'<ul><li>For example type AR, if you would like to see multiple genes, separate them by a comma: AR,FOXA1,MYCN</li> <li>Typing in a gene will also create a heatmap for that gene that maches up with the main heatmap.</li> <li>Selecting the "Sort gene rows by correlation with GOI?" option will run a pearson R correlation for all genes in the gene set versus your selected gene of interest.  The heatmap rows will be resorted by correlation with your GOI from low to high Pearson R</li></ul>',
+				'<li style="margin:10px 0";>The "Genes" panel provides a table of the gene order in the heatmap and the Pearson R if correlation sort is selected.</li>',
+				'<li style="margin:10px 0";>The "Excluded Genes" panel provides a table of the genes from your list that were not found in the selected database.</li>',
+				'<li style="margin:10px 0";>The generated heatmaps and data can be downloaded by navigating to the bottom of the left sidebar and selecting which data to download.</li>',
+			'</ol>',	
+		'</b>'
+		)}
+	)
+	
+	output$text4 <- renderText(
+		{ paste( 
+		'<b>',
+			'<ol>',
+				'<li style="margin:20px 0";>Format your expression data according to specifications below.</li>',
+			'</ol>',	
+		'</b>'
+		)}
+	)
+	
+	output$text5 <- renderText(
+		{ paste( 
+		'<p>',
+			'<ul>',
+				'<li style="margin:10px 0";>The first row must contain headers (See Row 1 to the left).</li>',
+				'<li style="margin:10px 0";>The first column (Column A) must contain gene names (gene symbols).</li>',
+				'<b><li style="margin:10px 0";>The second column must contain a group number, see the color key to choose a group color label.</li></b>',
+				'<li style="margin:10px 0";>Your expression measurements can be in multiple formats (RPKM, Log2, Z-Score, Normalized Microarray).  Make sure to adjust the heatmap scale accordingly!</li>',
+			'</ul>',	
+		'</p>'
+		)}
+	)
+	
+	output$text6 <- renderText(
+		{ paste( 
+		'<b>',
+			'<ol start="2">',
+				'<li style="margin:10px 0";>Once your data is formatted, upload the .txt file in the same way you would a gene list.</li>',
+				'<li style="margin:10px 0";>Under the "MyData Heatmap Options", choose to "Yes" under "Heatmap from Input?"</li>',
+				'<li style="margin:10px 0";>Navigate to the "MyData Heatmap" Tab, by default the genes will be in the order you provided.</li>',
+				'<li style="margin:10px 0";>You can cluster the genes by choosing "Cluster Rows" = "Yes".</li>',
+				'<li style="margin:10px 0";>Choosinge a normalization method:</li>',
+				'<ul><li>If your data is in RPKM/FPKM or raw normalized values from a microarray experiment you will probably want to choose "Z-Score" or "Log2" as a normalization method.</li> <li>If your data is already in Log2 or Z-Score format, you should choose "Raw".</li> <b><li>IMPORTANT: If you choose Log2, you should adjust the heatmap scale to have a low value of 0!!! The method is a log2(data + 1) adjustment, log2(0+1) = 0! </li></b></ul>',
+				'<li style="margin:10px 0";>The generated heatmaps and data can be downloaded by navigating to the bottom of the left sidebar and selecting "MyData Download Heatmap".</li>',
+			'</ol>',	
+		'</b>'
+		)}
+	)
+	
+	# Import Database, based on user choice
 	databaseInput <- reactive({
 		validate(
     		need(input$database != "Select Database...", "Select which database you would like to analyze") 
@@ -98,6 +181,7 @@ server <- function(input,output) {
 		return(tbl)
 	})
 	
+	#Upload user file
 	datasetInput <- reactive({
 		validate(
     		need(input$file1 != 0, "To begin drawing a heatmap, please select a file for input") 
@@ -108,7 +192,8 @@ server <- function(input,output) {
 		tbl <- as.data.frame(fread(inFile$datapath, sep="\t", header=TRUE, stringsAsFactors=FALSE))
 		return(tbl)
 	})
-		
+	
+	#User input Gene of Interest
 	geneInput <- reactive({
 		validate(
     		need(input$GOI != "", "Enter a gene of interest, or genes seperated by commas (i.e. FOXA1,AR,CXCR7)") 
@@ -118,12 +203,20 @@ server <- function(input,output) {
 			return(NULL)
 		return(inGOI)
 	})
-		
+	
+	#Does user want to resort heatmap by correlation with gene of interest?
 	correlInput <- reactive({
 		inCorrel <- input$correlationSort
 		return(inCorrel)
-	})	
+	})
+
+	#Make heatmap of user uploaded data
+	doInputHeat <- reactive({
+		doHeat <- input$doUserHeatmap
+		return(doHeat)
+	})
 	
+	#Draw the main heatmap using database selected
 	drawHeatmap <- function() {
 		selectDB <- databaseInput()
 		query <- datasetInput()
@@ -170,6 +263,7 @@ server <- function(input,output) {
 			heatmap.2(mat.dbq, scale="none", trace="none", dendrogram="none", breaks=seq(scalelow, scalehigh, length.out=101), Rowv=FALSE, Colv=FALSE, col=palette, ColSideColors=group, keysize=0.75, key.par=list(cex=0.5), labCol=FALSE, cexRow=cexRow)
 	}
 
+	#Draw heatmap from gene of interest
 	drawGeneHeatmap <- function() {
 		query <- datasetInput()
 		GOI <- geneInput()
@@ -208,14 +302,92 @@ server <- function(input,output) {
 			heatmap.2(mat.dbq, scale="none", trace="none", dendrogram="none", breaks=seq(scalelow, scalehigh, length.out=101), Rowv=FALSE, Colv=FALSE, col=palette, ColSideColors=group, keysize=0.75, key.par=list(cex=0.5), labCol=FALSE, cexRow=cexRow)		
 	}
 	
+	#Draw user heatmap from uploaded data
+	drawUserHeatmap <- function() {
+		#Run only if user checks box to ask:
+		if (doInputHeat() == "Yes") {
+			query <- datasetInput()
+			#GOI <- geneInput()
+			#GOI <- unlist(strsplit(GOI, ","))
+			group = c(rep("green", sum(query[1,] == -2)), rep("orange", sum(query[1,] == -1)), rep("black", sum(query[1,] == 0)), rep("red", sum(query[1,] == 1)), rep("darkred", sum(query[1,] == 2)))
+			#Process from raw values to z-score
+			rawToZscore <- function(raw) {
+				#Calculate Z-Score: For each row - (Cell - mean)/SD = Z-score units
+				row.means = rowMeans(raw[,2:ncol(raw)], na.rm=TRUE)
+				row.sd = apply(raw[,2:ncol(raw)], 1, function(x) sd(x, na.rm=TRUE))
+				raw[,2:ncol(raw)] = (raw[,2:ncol(raw)] - row.means)/row.sd
+				zscore = rbind(raw[2:nrow(raw),])
+				return(zscore)
+			}
+			
+			rawToRaw <- function(raw) {
+				#Just get rid of group row
+				raw <- raw[2:nrow(raw), ]
+				return(raw)
+			}
+			
+			rawToLog2 <- function(raw) {
+				#Just transform data to log2 + 1
+				raw[2:nrow(raw), 2:ncol(raw)] <- log2(raw[2:nrow(raw), 2:ncol(raw)] + 1)
+				#Already got groups so drop groups
+				log2 <- raw[2:nrow(raw),]
+				return(log2)
+			}
+			
+			#Create matrix and options for heatmap - run as function
+			returnHeat <- function(df) {
+				#Get matrix
+				mat = as.matrix(df[,2:ncol(query)])
+				rownames(mat) = df[,1]
+				#Heatmap options:
+				palette = colorRampPalette(c(input$lowcolor, input$midcolor, input$highcolor))(n=100)
+				scalelow = input$range[1]
+				scalehigh = input$range[2]
+				cexRow = as.numeric(as.character(input$xfontsize))
+				#Cluster rows?
+				if (input$userCluster == "Yes") {
+					Rowv=TRUE
+					dendrogram="row"
+				} else {
+					Rowv=FALSE
+					dendrogram="none"
+				}
+				#Run Heatmap:
+				heatmap.2(mat, scale = "none", trace="none", dendrogram=dendrogram, breaks=seq(scalelow, scalehigh, length.out=101), Rowv=Rowv, Colv=FALSE, col=palette, ColSideColors=group, keysize=0.75, key.par = list(cex=0.5), labCol=FALSE, cexRow=cexRow)
+			}
+			
+			processData <- function(df) {
+				#Normalize data based on choices to user: "Z-Score", "Raw", "Log2"
+				if (input$normalize == "Z-Score") {
+					df <- rawToZscore(df)
+				} else if (input$normalize == "Raw") {
+					df <- rawToRaw(df)
+				} else if (input$normalize == "Log2") {
+					df <- rawToLog2(df)
+				}
+				return(df)			
+			}
+			query <- processData(query)	
+			returnHeat(query)
+		}
+	}
+	
+	#Output plot1, database heatmap
 	output$plot1 <- renderPlot({
 		drawHeatmap()
 	})
 	
+	#Output plot2 Gene(s) of interest heatmap
 	output$plot2 <- renderPlot({
 		drawGeneHeatmap()
 	})
+	
+	#Output plot3, user heatmap of uploaded data
+	output$plot3 <- renderPlot({
+		drawUserHeatmap()
+	})
 
+	#Table 1 = "Genes included" tab, show genes that matched from user upload list to selected database
 	output$table1 <- renderTable({
 		query <- datasetInput()
 		selectDB <- databaseInput()
@@ -250,6 +422,7 @@ server <- function(input,output) {
 			if (correlate == FALSE) {return(cbind(Genes=rownames(mat.dbq)))}
 	})
 
+	#Table 2 - Genes not found from user list in selected database
 	output$table2 <- renderTable({
 		query <- datasetInput()
 		selectDB <- databaseInput()
@@ -257,23 +430,44 @@ server <- function(input,output) {
 		return(cbind(Genes= subset(query[,1], !(query[,1] %in% selectDB[,"Gene_name"]))))
 	})
 	
-	output$downloadHeatmap <- downloadHandler(
-	filename <- function() {
-		paste0(
-			basename(unlist(strsplit(input$file1$name, split='.txt', fixed=TRUE))[1]), #Get name of file minus .txt
-			input$database,
-			'-heatmap',
-			'.png', 
-			sep=''
-			)
-		},
-	content <- function(file) {
-		png(file, width = 6, height = 10, units = "in", res = 300)
-		drawHeatmap()
-		dev.off()
+	#Download user heatmap - "MyData")
+	output$downloadUserHeatmap <- downloadHandler(
+		filename <- function() {
+			paste0(
+				basename(unlist(strsplit(input$file1$name, split='.txt', fixed=TRUE))[1]), #Get name of file minus .txt
+				"_",
+				input$normalize,
+				'_heatmap',
+				'.png', 
+				sep=''
+				)
+			},
+		content <- function(file) {
+			png(file, width = 6, height = 10, units ="in", res = 300)
+			drawUserHeatmap()
+			dev.off()
 		}
 	)
 	
+	#Download main heatmap
+	output$downloadHeatmap <- downloadHandler(
+		filename <- function() {
+			paste0(
+				basename(unlist(strsplit(input$file1$name, split='.txt', fixed=TRUE))[1]), #Get name of file minus .txt
+				input$database,
+				'-heatmap',
+				'.png', 
+				sep=''
+				)
+			},
+		content <- function(file) {
+			png(file, width = 6, height = 10, units = "in", res = 300)
+			drawHeatmap()
+			dev.off()
+			}
+	)
+	
+	#Download gene of interest heatmap
 	output$downloadGeneHeatmap <- downloadHandler(
 		filename <- function() {
 			paste0(
@@ -294,6 +488,7 @@ server <- function(input,output) {
 		}
 	)
 	
+	#Download data matrix, which is zscore data, for selected DB (matched genes from user list)
 	output$downloadDataMatrix <- downloadHandler(
 		filename <- function() {
 			paste0(
@@ -347,6 +542,7 @@ server <- function(input,output) {
 		}
 	)
 	
+	#Download zscore matrix for gene(s) of interest
 	output$downloadGOIMatrix <- downloadHandler(
 		filename <- function() {
 			paste0(
@@ -389,6 +585,7 @@ server <- function(input,output) {
 		}
 	)
 	
+	#Tab of table with database summaries - rendered with "datatable" to jazz it up and provide user interaction
 	#Do Datasets summary table
 	databaseSummary <- as.data.frame(fread("Datasets-Summary.txt", sep="\t", stringsAsFactors=FALSE, header=TRUE))
 	
--- YuLab-SU2C-TCGA-HeatmapApp/ui.R
+++ YuLab-SU2C-TCGA-HeatmapApp/ui.R
@@ -15,6 +15,13 @@ pageWithSidebar(
 			multiple = FALSE
 			),
 		fileInput("file1", h3("Gene List File")),
+		h4("MyData Heatmap Options:"),
+		div(style="display: inline-block;vertical-align:top; width: 30%;",
+			selectInput("doUserHeatmap", "Heatmap from input?", choices = list("Yes", "No"), selected = "No", multiple = FALSE)),
+		div(style="display: inline-block;vertical-align:top; width: 30%;",
+			selectInput("normalize", "Normalization", choices = list("Z-Score", "Raw", "Log2"), selected = "Z-Score", multiple = FALSE)),
+		div(style="display: inline-block;vertical-align:top; width: 30%;",
+			selectInput("userCluster", "Cluster Rows", choices = list("No", "Yes"), selected = "No", multiple = FALSE)),
 		textInput("GOI", h3("Gene of interest to sort groups"), value = NULL, placeholder ="Input gene, i.e. AR"),
 		checkboxInput("correlationSort", h5("Sort gene rows by correlation with GOI?"), value = FALSE),
 		h3("Heatmap Options:"),
@@ -30,6 +37,7 @@ pageWithSidebar(
 			min = -10, max = 10, value = c(-2,2)
 		),
 		h3("Download Data:"),
+		downloadButton("downloadUserHeatmap", "Download My Data Heatmap"),
 		downloadButton("downloadHeatmap", "Download Heatmap"),
 		downloadButton("downloadGeneHeatmap", "Download Gene Heatmap"),
 		downloadButton("downloadDataMatrix", "Download heatmap data matrix for gene set"),
@@ -37,13 +45,36 @@ pageWithSidebar(
 	),
 	mainPanel(
 		tabsetPanel(
-		tabPanel("Heatmap", h4("Heatmap (Gene Z-score across all samples)"),plotOutput("plot1"), plotOutput("plot2")),
-		tabPanel("Genes", h4("Included Gene List (Ordered)"), tableOutput("table1")),
-		tabPanel("Excluded Genes", h4("Genes not found in selected database"), tableOutput("table2")),
-		tabPanel("Datasets Summaries", h4("Summaries of Database Datasets for Analysis:"), DT::dataTableOutput("tableSummary"))
-		)
+			tabPanel(
+				"Instructions", 
+				h3(strong("Heatmap App Instructions:"), align="center"),
+				htmlOutput("text1"),
+				div(style="display: inline-block;vertical-align:top; width: 5%;", ""),
+				div(style="display: inline-block;vertical-align:top; width: 20%;",	
+					img(src='gene_list.png', height = '100%', width='100%')),
+				div(style="display: inline-block;vertical-align:top; width: 70%;",
+					htmlOutput("text2")),
+				htmlOutput("text3"),
+				
+				h3(strong("Create a heatmap of your data!"), align="center"),
+				htmlOutput("text4"),
+				div(style="display: inline-block;vertical-align:middle; width: 4%;", ""),
+				div(style="display: inline-block;vertical-align:top; width: 35%;",	
+					img(src='ExampleDataset.png', height = '100%', width='100%')),
+				div(style="display: inline-block;vertical-align:top; width: 10%;",	
+					img(src='ColorKey.png', height = '100%', width='100%')),
+				div(style="display: inline-block;vertical-align:top; width: 45%;",
+					htmlOutput("text5")),
+				htmlOutput("text6")
+			), 
+			tabPanel("Heatmap", h4("Heatmap (Gene Z-score across all samples)", align="center"),plotOutput("plot1"), plotOutput("plot2")),
+			tabPanel("Genes", h4("Included Gene List (Ordered)"), tableOutput("table1")),
+			tabPanel("Excluded Genes", h4("Genes not found in selected database"), tableOutput("table2")),
+			tabPanel("MyData Heatmap", h4("Heatmap of Input Data (Gene Z-score across all samples)", align="center"),plotOutput("plot3")),
+			tabPanel("Datasets Summaries", h4(strong("Summaries of Database Datasets for Analysis:"), align="center"), DT::dataTableOutput("tableSummary"))
 		)
 	)
+)
 
 
 
